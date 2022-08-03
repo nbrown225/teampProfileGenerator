3b63ac729cfe2098570639bfc40357f0c39d75b6
nbrown225 <chbrown225@yahoo.com>
2022-08-03 06:43:02 -0700

    engineer github test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,20 @@
 const Engineer = require('../lib/Engineer');
 
 //create the engineer object
-test('engineer object should be created', () => {
-    const engineer = new Engineer('May', 223, 'may.engineew@jobname.net', 'github.com/may');
+test('engineer object WILL created', () => {
+    const engineer = new Engineer('Bill', 211, 'bill.engineew@jobname.net', 'github.com/bill');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.github).toEqual(expect.any(String));
+    expect(engineer.email).toEqual(expect.any(String));
+});
+
+// GITHUB
+test(`get github`, () => {
+    const engineer = new Engineer('Bill', 211, 'bill.engineew@jobname.net', 'github.com/bill');
+
+    expect(engineer.getName()).toEqual(expect.any(String));
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+
 });
