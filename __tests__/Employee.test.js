30a457e18c531a971c672bf36bc896c6e7b03e17
nbrown225 <chbrown225@yahoo.com>
2022-08-03 04:15:11 -0700

    get id test passed;

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -10,4 +10,15 @@ test(`create the employee object`, () => {
     expect(employee.github).toEqual(expect.any(String));
         // expect(employee.name).toBe(expect.any(String));
 
+});
+
+// --------EMPLOYEE ID ----------
+test(`get employee id`, () => {
+    const employee = new Employee('Bob', 222, 'bob@job.org', 'github.bob');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.getName()).toEqual(expect.any(String));
+    expect(employee.getId()).toEqual(expect.any(Number));
 })
+
