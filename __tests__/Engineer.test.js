f0e00da3f0d5aca17cdd4ca5d8fe22abee2c8c82
nbrown225 <chbrown225@yahoo.com>
2022-08-03 05:15:21 -0700

    engineer test passed;

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,6 @@
+const Engineer = require('../lib/Engineer');
+
+//create the engineer object
+test('engineer object should be created', () => {
+    const engineer = new Engineer('May', 223, 'may.engineew@jobname.net', 'github.com/may');
+});
