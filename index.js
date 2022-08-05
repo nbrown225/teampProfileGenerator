02033395ffff46b203786b93be53232daf57f0e9
nbrown225 <chbrown225@yahoo.com>
2022-08-05 07:32:29 -0700

    able to get an array from name input in engineer questions

--- index.js
+++ index.js
@@ -1,9 +1,89 @@
 var inquirer = require("inquirer");
-function test () {
-    inquirer.prompt({
-        type: 'name', 
-        name: 'name',
-        message: 'name?',
+
+const teamData = []
+function chooseRole () {
+    inquirer.prompt(
+// CHOOSE NEW ROLE
+        {
+        type: 'list',
+        name: 'role',
+        message: 'Choose employee role',
+        choices: ['Engineer', 'Intern', 'Manager'], 
+        validate: roleInput => {
+            if (roleInput){
+                testWorks();
+                return true;
+            } else{
+                console.log("Role must be chosen to continue");
+                return false
+            }
+        }
+    }
+    )
+    // CHECK ROLE / SWITCH TO CORRESPONDING FUNCTION
+    .then(({role}) => {
+        if (role == 'Engineer') {
+            addEngineer();
+        } else if (role == 'Intern'){
+            addIntern();
+        } else if (role == 'Manager'){
+            addManager();
+        }
     })
 }
-test();
+// ENGINEER
+function addEngineer() {
+    inquirer.prompt(
+        questions = [
+            {
+                type: 'text', 
+                name: 'newname',
+                message: 'enter name',
+                
+            }
+        ]
+    )
+    // CHECK IF NAME WAS ENTERED
+    .then((answers) =>{
+        let {newname} = answers;
+        const engineer = (newname);
+        teamData.push(engineer);
+        console.log(teamData);
+
+    })
+}
+// INTERN
+function addIntern() {
+    inquirer.prompt(
+        questions = [
+            {
+                type: 'text', 
+                name: 'newname',
+                message: 'enter name',
+                
+            }
+        ]
+    )
+    .then((answer) => {
+        console.log('hello' + answer.newname);
+        var {newname} = answer
+    })
+}
+// MANAGER
+function addManager() {
+    inquirer.prompt(
+        questions = [
+            {
+                type: 'text', 
+                name: 'newname',
+                message: 'enter name',
+                
+            }
+        ]
+    )
+    .then((answer) => {
+        console.log('hello' + answer.newname);
+
+    })
+}
+chooseRole();
