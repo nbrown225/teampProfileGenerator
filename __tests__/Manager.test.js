0200079147946c50b00c7ed818188856ac0c383a
nbrown225 <chbrown225@yahoo.com>
2022-08-05 03:30:25 -0700

    manager tests passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,21 @@
-const Manager = require('../lib/Manager');
-
-test(`create manager object`, () => {
-    const manager = new Manager('Ben', 411, 'ben@job.com', 1);
+const Manager = require('../lib/Manager')
+test('create an manager object', () => {
+    const manager = new Manager('Niq', 7, 'email@email.com', 69);
 
     expect(manager.name).toEqual(expect.any(String));
     expect(manager.id).toEqual(expect.any(Number));
     expect(manager.email).toEqual(expect.any(String));
     expect(manager.building).toEqual(expect.any(Number));
+});
+
+test('get office number', () => {
+    const manager = new Manager('Niq', 7, 'email@email.com', 69);
+
+    expect(manager.getBuilding()).toEqual(69);
+});
+
+test('get role', () => {
+    const manager = new Manager('Niq', 7, 'email@email.com', 69);
+
+    expect(manager.getRole()).toEqual('Manager');
 });
