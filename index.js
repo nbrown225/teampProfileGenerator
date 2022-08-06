a643cbff89c30118fb67a2294f5e1dadc4c00355
nbrown225 <chbrown225@yahoo.com>
2022-08-06 00:16:42 -0700

    able to get page to generate-

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
 // GET NODE MODULE
 var inquirer = require("inquirer");
 const fs = require('fs');
-
+const generateHTML = require('./src/generateHTML');
 // TEAM PROFILES
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+let engineerQuestions = [
+    {
+        type: 'text', 
+        name: 'name',
+        message: 'Enter name (REQUIRED)',
+        validate: nameInput => {
+            if (nameInput){
+                return true;
+            } else {
+                console.log('Name is REQUIRED to continue');
+                return false;
+            }
+        }
+        
+    },
+    {
+        type: 'text', 
+        name: 'id',
+        message: 'Enter ID number (REQUIRED)',
+        validate: idInput=(value)=>{
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'ID number is REQUIRED!';
+        }
+    },
+    {
+        type: 'text', 
+        name: 'email',
+        message: 'Enter email (REQUIRED)',
+        validate: emailInput => {
+            if(emailInput){
+                return true;
+            } else{
+                console.log('Email is REQUIRED to continue');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'text',
+        name: 'github',
+        message: 'Enter Github (REQUIRED)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else{
+                console.log('Github is REQUIRED to continue');
+                return false;
+            }
+        }
+    }
+];
 
-const teamData = [];
+
+const teamMembers = [];
 function chooseRole () {
     console.log(
         '============= ADD NEW EMPLOYEE ==================='
@@ -44,89 +96,29 @@ function chooseRole () {
 }
 // ENGINEER
 function addEngineer() {
-    console.log(
-        '============= Engineer Criteria ==================='
+    inquirer.prompt(engineerQuestions).then((engineerAnswers)=>{
+    console.log(engineerAnswers);
+    const engineer = new Engineer(
+        engineerAnswers.name,
+        engineerAnswers.id,
+        engineerAnswers.email,
+        engineerAnswers.github
     );
-    inquirer.prompt(
-        questions = [
-            {
-                type: 'text', 
-                name: 'name',
-                message: 'Enter name (REQUIRED)',
-                validate: nameInput => {
-                    if (nameInput){
-                        return true;
-                    } else {
-                        console.log('Name is REQUIRED to continue');
-                        return false;
-                    }
-                }
-                
-            },
-            {
-                type: 'text', 
-                name: 'id',
-                message: 'Enter ID number (REQUIRED)',
-                validate: idInput=(value)=>{
-                    var valid = !isNaN(parseFloat(value));
-                    return valid || 'ID number is REQUIRED!';
-                }
-            },
-            {
-                type: 'text', 
-                name: 'email',
-                message: 'Enter email (REQUIRED)',
-                validate: emailInput => {
-                    if(emailInput){
-                        return true;
-                    } else{
-                        console.log('Email is REQUIRED to continue');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'text',
-                name: 'github',
-                message: 'Enter Github (REQUIRED)',
-                validate: githubInput => {
-                    if (githubInput) {
-                        return true;
-                    } else{
-                        console.log('Github is REQUIRED to continue');
-                        return false;
-                    }
-                }
-            }
-        ]
-    )
-    // CHECK IF NAME WAS ENTERED
-    .then((answers) =>{
-        //console.log(answers);
-        let {name, id, email, github, role} = answers
-        teamData.push(answers);
-        const engineer = new Engineer (name, id, email, github, role)
-        //console.log(teamData);
-        console.log(engineer);
-        writeFile()
-
-
+    teamMembers.push(engineer);
+    console.log(teamMembers);
     })
+    return writeToFile(teamMembers)
 }
-
-function writeFile (data){
-    fs.writeFile('./dist/test.html', data, err=>{
+function writeToFile(data){
+    fs.writeFile('./dist/testthis.html', generateHTML(data), 'utf-8', err =>{
         if (err) {
             console.log(err);
-            return
+            return;
         } else {
-            console.log('created');
+            console.log('succes');
         }
     })
 }
 
-function addMore(){
-
-}
 
 chooseRole();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,25 +1,83 @@
-const fs = require('fs');
 
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-const Manager = require("../lib/Manager");
 
-function generatePage () {
-    return
+
+const cardArr = [];
+// create Engineer card
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
     `
-    `<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8"></meta>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <link rel='stylesheet' href='./dist/style.css'></link>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
-            <title>TEAMS</title>
-        </head>
-        <header class="card-panel pink lighten-2">
-        <h1 class="center">
-            My Team
-        </h1>
-    </header>
-    </html>
 }
+    if (engineer){
+        engineerCard = generateEngineer(engineer);
+        cardArr.push(engineerCard)
+    }
+
+
+// generate html page 
+const generateHTML = function (cardArr) {  
+  return`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <link rel="stylesheet" href="./style.css">
+      <title>Teams</title>
+  </head>
+  <body>
+      <header class="card-panel pink lighten-2">
+          <h1 class="center">
+              My Team
+          </h1>
+      </header>
+      <main class="card-panel teal center">
+      <div class="row s12 center">
+        ${cardArr}
+          <div class="col s3 card-panel" id="intern-card">
+              <div class="card-title">
+                  <h2 id="name">Bob</h2>
+              </div>
+              <div class="card-content">
+                  <h4 id="role">Intern</h4>
+                  <p id="id">3301</p>
+                  <p id="email">bob@job.com</p>
+                  <p id="school">UCAU</p>
+              </div>
+          </div>
+          <div class="col s3 card-panel" id="manager-card">
+              <div class="card-title">
+                  <h2 id="name">Ryan</h2>
+              </div>
+              <div class="card-content">
+                  <h4 id="role">Manager</h4>
+                  <p id="id">3331</p>
+                  <p id="email">ryan@job.com</p>
+                  <p id="building">222</p>
+              </div>
+          </div>
+
+      </div>
+
+  </main>
+</body>
+</html>
+`;
+}
+
+// export to index
+module.exports = generateHTML; 
